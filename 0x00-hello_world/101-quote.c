f281c7de02b5bf97b83d368ae38cc168182080a8
kenneth377 <kobbie377@gmail.com>
2023-06-15 17:19:12 -0700

    More work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,8 @@
 
 int main(void)
 {
-	char n[] = "Dora Korpar";
 
-	fputs("and that piece of art is useful\" -%s, 2015-10-19\n", n, stdout);
+
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
