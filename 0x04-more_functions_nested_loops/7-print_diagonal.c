6f5bfcda3cc0ff145dcba0cde404f31a9a3240d1
kenneth377 <kobbie377@gmail.com>
2023-06-22 17:57:22 -0700

    We moving diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -5,28 +6,26 @@
  * @n: number of times the character \ should be printed
  */
 
-
 void print_diagonal(int n)
 {
-	int i;
-	int j;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			j = 1;
-			for (j = 1; j <= i; j++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar(' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
-			_putchar('\\');
 			_putchar('\n');
 		}
-
 	}
 }
